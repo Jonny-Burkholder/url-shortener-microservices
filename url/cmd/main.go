1cc7b153c587bde03e90f0b0525beea260aa017c
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2022-02-28 19:31:56 -0500

    Update main.go

--- url/cmd/main.go
+++ url/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,13 +11,28 @@ func main() {
 
 	log.Println("Now serving on port 4040")
 
-	http.HandleFunc("/get-shorty/", handleGetShorty)
+	http.HandleFunc("/get-shorty", handleGetShorty)
 
 	http.ListenAndServe(":4040", nil)
 
 }
 
 func handleGetShorty(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Welcome!")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("content-type", "application/json;utf=8")
+	data := "get.shorty/1"
+	b, err := json.Marshal(map[string]interface{}{
+		"api": map[string]interface{}{
+			"name":    "get shorty",
+			"version": 0.1,
+			"data":    data,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(r.Method)
 	switch r.Method {
 	case http.MethodPost:
 		//do stuff
@@ -24,4 +40,5 @@ func handleGetShorty(w http.ResponseWriter, r *http.Request) {
 	default:
 		http.Error(w, "Unable to process request", http.StatusBadRequest)
 	}
+	fmt.Fprintf(w, "%s", b)
 }
