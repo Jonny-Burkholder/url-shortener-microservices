cb82a1fa5143545991960e244eb1eab57a6b0ce3
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2022-02-28 20:50:39 -0500

    Update main.go
    ???

--- url/cmd/main.go
+++ url/cmd/main.go
@@ -17,25 +17,27 @@ func main() {
 
 }
 
-func handleGetShorty(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("content-type", "application/json;utf=8")
-	data := "get.shorty/1"
-	b, err := json.Marshal(data)
-	if err != nil {
-		panic(err)
+func handleGetShorty(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("content-type", "application/json;utf=8")
+		data := "get.shorty/1"
+		b, err := json.Marshal(data)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("HTTP Method:")
+		fmt.Println(r.Method)
+		fmt.Println("printing url body:")
+		fmt.Println(r.Body)
+		switch r.Method {
+		case http.MethodPost:
+			//do stuff
+		default:
+			log.Println("Error!")
+			http.Error(w, "Unable to process request", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "%s", b)
 	}
-	fmt.Println("HTTP Method:")
-	fmt.Println(r.Method)
-	fmt.Println("printing url body:")
-	fmt.Println(r.Body)
-	switch r.Method {
-	case http.MethodPost:
-		//do stuff
-	default:
-		log.Println("Error!")
-		http.Error(w, "Unable to process request", http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(w, "%s", b)
 }
