8337bb2cb953081941255af347ef3104ccb31f51
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2022-03-01 12:02:31 -0500

    Update main.go

--- url/cmd/main.go
+++ url/cmd/main.go
@@ -17,29 +17,32 @@ func main() {
 
 }
 
-func handleGetShorty(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("content-type", "application/json;utf=8")
-		data := "get.shorty/1"
-		b, err := json.Marshal(data)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("HTTP Method:")
-		fmt.Println(r.Method)
-		fmt.Println("printing url body:")
-		fmt.Println(r.Body)
-		switch r.Method {
-		case http.MethodOptions:
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			fn(w, r)
-		case http.MethodPost:
-			//do stuff
-		default:
-			log.Println("Error!")
-			http.Error(w, "Unable to process request", http.StatusBadRequest)
-			return
-		}
+func setupCORSResponse(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func handleGetShorty(w http.ResponseWriter, r *http.Request) {
+	setupCORSResponse(&w)
+	data := "get.shorty/1"
+	b, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("HTTP Method:")
+	fmt.Println(r.Method)
+	fmt.Println("printing url body:")
+	fmt.Println(r.Body)
+	switch r.Method {
+	case http.MethodOptions:
+		return
+	case http.MethodPost:
 		fmt.Fprintf(w, "%s", b)
+		return
+	default:
+		log.Println("Error!")
+		http.Error(w, "Unable to process request", http.StatusBadRequest)
+		return
 	}
 }
